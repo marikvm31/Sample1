e552065e45b0debdbdb12df1a5a334d3166cefc7
marikvm31 <156293567+marikvm31@users.noreply.github.com>
2024-01-11 13:22:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './components/Hari';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>New React App</h1>
+      </header>
+        <Welcome />
+        {/* <Gmail/> */}
+    </div>
+  );
+}
+
+export default App;
